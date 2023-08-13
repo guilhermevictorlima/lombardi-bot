b777f50f0d5eb5af10b915414a91e6499f1b1372
Guilherme Lima <guilhermevictorrocha@gmail.com>
2023-08-13 00:39:15 -0300

    feat(commands): using command abstraction to build all commands on discord api startup

--- src/main/java/com/github/guilhermevictorlima/lombardi/api/LombardiBotConfiguration.java
+++ src/main/java/com/github/guilhermevictorlima/lombardi/api/LombardiBotConfiguration.java
@@ -1,5 +1,6 @@
 package com.github.guilhermevictorlima.lombardi.api;
 
+import com.github.guilhermevictorlima.lombardi.api.commands.BotCommand;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.javacord.api.DiscordApi;
 import org.javacord.api.DiscordApiBuilder;
@@ -9,6 +10,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
+
 @Configuration
 public class LombardiBotConfiguration {
 
@@ -17,22 +20,28 @@ public class LombardiBotConfiguration {
     private static final String DISCORD_TOKEN_ENV_NAME = "DISCORD_APPLICATION_TOKEN";
 
     @Bean
-    public DiscordApi discordApi() {
+    public DiscordApi discordApi(List<BotCommand> commands) {
         LOGGER.info("Starting Discord API authentication...");
+        final DiscordApi discordApi = authenticateApi();
+        LOGGER.info("Discord API successfully authenticated!");
 
+        LOGGER.info("Creating slash commands and their listeners...");
+        commands.forEach(command -> command.buildCommand(discordApi));
+        LOGGER.info("Slash commands and listeners successfully created!");
+
+        return discordApi;
+    }
+
+    private DiscordApi authenticateApi() {
         final String token = Dotenv.configure()
                 .load()
                 .get(DISCORD_TOKEN_ENV_NAME);
 
-        final DiscordApi discordApi = new DiscordApiBuilder()
+        return new DiscordApiBuilder()
                 .setToken(token)
                 .setIntents(Intent.GUILD_VOICE_STATES)
                 .login()
                 .join();
-
-        LOGGER.info("Discord API successfully authenticated!");
-
-        return discordApi;
     }
 
 }
