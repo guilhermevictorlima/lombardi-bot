c2a3172c9c0679b9b89153887678916248431b9b
Guilherme Lima <guilhermevictorrocha@gmail.com>
2023-08-13 00:37:32 -0300

    feat(commands): creating command abstraction

---
+++ src/main/java/com/github/guilhermevictorlima/lombardi/api/commands/PlayCommand.java
@@ -0,0 +1,40 @@
+package com.github.guilhermevictorlima.lombardi.api.commands;
+
+import org.javacord.api.interaction.SlashCommandInteraction;
+import org.javacord.api.interaction.SlashCommandOption;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.List;
+
+// TODO remover parâmetros de teste e iniciar implementação do comando
+@Service
+public class PlayCommand extends BotCommand {
+
+    @Override
+    protected void execute(SlashCommandInteraction interaction) {
+        interaction.createImmediateResponder()
+                .setContent("Teste")
+                .respond();
+    }
+
+    @Override
+    public String getName() {
+        return "play";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Toque uma música";
+    }
+
+    @Override
+    public List<SlashCommandOption> getOptions() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    protected boolean isSatisfiedBy(SlashCommandInteraction interaction) {
+        return interaction.getCommandName().equals(getName());
+    }
+}
